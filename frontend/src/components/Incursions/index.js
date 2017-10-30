ae3493a4fb0ab316fa904e83557a1c1d67caa7d8
msaracevic <msaracevic@users.noreply.github.com>
2017-10-30 23:10:27 +0100

    Feat: Basic incursion FE files

--- frontend/src/components/Incursions/index.js
+++ frontend/src/components/Incursions/index.js
@@ -1,11 +1,154 @@
 import React, {Component} from 'react';
+import LoadingScreen from '../pages/LoadingScreen';
 
-export default class Incursions extends Component {
-  render() {
+function systemsLayout(systems) {
+  return systems.map(system => {
     return (
-      <div>
-        Incursion Manager
+      <div style={{paddingLeft: 3 + 'em'}} key={system.solarSystemID}>
+        {system.solarSystemName} {system.incursionType}
       </div>
     );
+  });
+}
+
+function incursions(incursions) {
+  return Object.keys(incursions).map(key => {
+    const incursion     = incursions[key],
+          aggressor     = incursion['aggressorFactionID']['name'],
+          aggressorId   = incursion['aggressorFactionID']['id'],
+          factionId     = incursion['info']['factionID'],
+          factionName   = incursion['info']['factionName'],
+          constellation = incursion['info']['constellationName'],
+          influence     = incursion['influence'],
+          state         = incursion['state'],
+          hasBoss       = incursion['hasBoss'],
+          systems       = incursion['info']['systems'];
+
+    const aggressorImg     = `https://image.eveonline.com/Alliance/${aggressorId}_128.png`,
+          constellationImg = `https://image.eveonline.com/Alliance/${factionId || 500000}_128.png`;
+
+    return (
+      <article key={incursion['constellation']['id_str']}>
+        {constellation} <img src={constellationImg} alt={constellation}/> <br/>
+        {systemsLayout(systems)}
+      </article>
+    );
+  });
+}
+
+export default class Incursions extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      incursions: {}
+    };
+  };
+
+  componentWillMount() {
+    fetch('/api/get-incursions')
+      .then(response => response.json())
+      .then(response => {
+        this.setState({
+          incursions: response
+        });
+        console.log(response);
+      });
+  }
+
+  render() {
+    if (Object.keys(this.state.incursions).length === 0) {
+      return <LoadingScreen/>;
+    } else {
+      return (
+        <section className="incursions">
+          <h1>Incursions</h1>
+          {incursions(this.state.incursions)}
+        </section>
+      );
+    }
   }
 }
+
+/*
+{
+  "info": {
+    "constellationID": 20000643,
+    "constellationName": "T-RQ7S",
+    "factionName": null,
+    "factionID": null,
+    "systems": [
+      {
+        "solarSystemID": 30004402,
+        "solarSystemName": "QYZM-W",
+        "radius": "28.6",
+        "security": "-1.0",
+        "constellationID": 20000643,
+        "hasRepairs": false,
+        "stations": [],
+        "incursionType": "N/A"
+      },
+      {
+        "solarSystemID": 30004400,
+        "solarSystemName": "I-7JR4",
+        "radius": "33.3",
+        "security": "-0.4",
+        "constellationID": 20000643,
+        "hasRepairs": false,
+        "stations": [],
+        "incursionType": "N/A"
+      },
+      {
+        "solarSystemID": 30004398,
+        "solarSystemName": "ME-4IU",
+        "radius": "15.7",
+        "security": "-0.6",
+        "constellationID": 20000643,
+        "hasRepairs": false,
+        "stations": [],
+        "incursionType": "N/A"
+      },
+      {
+        "solarSystemID": 30004401,
+        "solarSystemName": "CH9L-K",
+        "radius": "11.9",
+        "security": "-0.5",
+        "constellationID": 20000643,
+        "hasRepairs": false,
+        "stations": [],
+        "incursionType": "N/A"
+      },
+      {
+        "solarSystemID": 30004403,
+        "solarSystemName": "3KNA-N",
+        "radius": "29.9",
+        "security": "-0.5",
+        "constellationID": 20000643,
+        "hasRepairs": false,
+        "stations": [],
+        "incursionType": "N/A"
+      },
+      {
+        "solarSystemID": 30004399,
+        "solarSystemName": "BU-IU4",
+        "radius": "7.2",
+        "security": "-0.9",
+        "constellationID": 20000643,
+        "hasRepairs": false,
+        "stations": [],
+        "incursionType": "N/A"
+      },
+      {
+        "solarSystemID": 30004397,
+        "solarSystemName": "9-B1DS",
+        "radius": "15.2",
+        "security": "-0.5",
+        "constellationID": 20000643,
+        "hasRepairs": false,
+        "stations": [],
+        "incursionType": "N/A"
+      }
+    ]
+  }
+}
+ */
--- server/app/incursions/index.js
+++ server/app/incursions/index.js
@@ -4,7 +4,29 @@ import * as Incursions from '../../models/incursions';
 function getIncursionData(activeIncursions) {
   let promiseArray = activeIncursions.map(incursion =>
     IncursionMap.findById(incursion.constellation.id).then(constellation => {
+      const islands  = ['Goins', 'Mayonhen', 'Orvanne', 'Unour', 'Pietanen'],
+            result   = [];
+      
       incursion.info = constellation.dataValues.value;
+      delete incursion.infestedSolarSystems;
+
+      // for (var system = 0; system < incursion.systems.length; system++) {
+      //   incursion.systems[system].systemRadius = Math.round(incursion.systems[system].systemRadius, 0);
+      //
+      //   if (incursion.systems[system].incursionType == 'HQ') incursion.systems[system].typeSort = 0;
+      //   else if (incursion.systems[system].incursionType == 'AS') incursion.systems[system].typeSort = 1;
+      //   else if (incursion.systems[system].incursionType == 'VG') incursion.systems[system].typeSort = 2;
+      //   else if (incursion.systems[system].incursionType == 'ST') incursion.systems[system].typeSort = 3;
+      //   else if (incursion.systems[system].incursionType == 'NA') incursion.systems[system].typeSort = 4;
+      //
+      //   // check if lowsec system
+      //   if (incursion.systems[system].systemSecurity < 0.5) {
+      //     numberOfLowsecSystems++;
+      //   } else if (incursion.systems[system].systemSecurity === 1) {
+      //     incursion.systems[system].systemSecurity = '1.0';
+      //   }
+      // }
+
       return incursion;
     })
   );
