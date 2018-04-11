2d1fff1c9cd6d7768e55e33c627903e3c521cb7b
Miroslav Saracevic <msaracevic@users.noreply.github.com>
2018-04-11 13:24:04 +0200

    Fix: Open CORS to wildcard

--- server/middleware/cors.js
+++ server/middleware/cors.js
@@ -1,5 +1,5 @@
 export default function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'localhost');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
