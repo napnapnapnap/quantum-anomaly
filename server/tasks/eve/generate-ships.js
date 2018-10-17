28c2cf7fc2ade62860fb0a3a140bed02ce0d2227
msaracevic <msaracevic@users.noreply.github.com>
2018-10-18 00:44:41 +0200

    Refactor: Comment and optimize ship table generation code

--- server/tasks/eve/generate-ships.js
+++ server/tasks/eve/generate-ships.js
@@ -2,29 +2,35 @@ import * as logger from '../../helpers/logger';
 import {models} from '../../models';
 
 export default function () {
-  let shipTypesBulk = [],
-      shipIds       = [],
-      shipsBulk     = [];
+  let shipGroupsBulk = [],
+      shipIds        = [],
+      shipsBulk      = [];
 
-  return models.EveMarketGroups.findAll().then(marketGroups => {
-    shipTypesBulk = marketGroups
-      .filter(marketGroup => marketGroup.data.category_id === 6 && marketGroup.data.published)
-      .map(marketGroup => {
-        shipIds = shipIds.concat(marketGroup.data.types);
+  return models.EveGroups.findAll().then(groups => {
+    // first generate ship groups in following form
+    // { id: 6,
+    //   name: 'Strategic Cruiser',
+    //   data: [ 29984, 29986, 29988, 29990 ]
+    // }
+    // where data is the ID's of the ship which are in this group
+    // just take ID=6 which marks ship market groups and are published
+    // also remember all the actuall ship ID's we encounter
+    shipGroupsBulk = groups
+      .filter(group => group.data.category_id === 6 && group.data.published)
+      .map(group => {
+        shipIds = shipIds.concat(group.data.types);
         return {
-          id:        marketGroup.data.group_id,
-          name:      marketGroup.data.name,
-          data:      marketGroup.data.types,
+          id:        group.data.group_id,
+          name:      group.data.name,
+          data:      group.data.types,
           createdAt: new Date(),
           updatedAt: new Date()
         };
-        // { id: 6,
-        //   name: 'Strategic Cruiser',
-        //   data: [ 29984, 29986, 29988, 29990 ] }
       });
   }).then(() => {
+    // go through each ship ID's we encountered and get the full model for it
     return Promise.all(shipIds.map(id =>
-      models.EveInvTypes.find({
+      models.EveTypes.find({
         where: {id: id}
       }).then(ship => {
         if (ship && ship.data.published) {
@@ -35,6 +41,7 @@ export default function () {
       })
     ));
   }).then(ships => {
+    // remove if there are any null data for one or the other reason
     shipsBulk = ships
       .filter(ship => ship !== null)
       .map(ship => {
@@ -46,9 +53,10 @@ export default function () {
           updatedAt: new Date()
         };
       });
-
-    // remove unpublished ships from shipTypesBulk
-    shipTypesBulk.forEach(shipType => {
+    // after all this information is avaiable, go back to the ship groups
+    // and attach real ship name to each ID, so we end up with options to later
+    // search ships via ID or by name
+    shipGroupsBulk.forEach(shipType => {
       shipType.data = shipType.data.map(id => {
         let shipName = null;
         shipsBulk.forEach(ship => {
@@ -67,14 +75,12 @@ export default function () {
   }).then(() => {
     return models.EveShipTypes.destroy({truncate: true});
   }).then(() => {
-    return models.EveShipTypes.bulkCreate(shipTypesBulk).then(() =>
-      logger.action(`Created ${shipTypesBulk.length} ship types`)
-    );
+    logger.action(`Creating ${shipGroupsBulk.length} groups of ships`);
+    return models.EveShipTypes.bulkCreate(shipGroupsBulk);
   }).then(() => {
     return models.EveShips.destroy({truncate: true});
   }).then(() => {
-    return models.EveShips.bulkCreate(shipsBulk).then(() =>
-      logger.action(`Created ${shipsBulk.length} ships`)
-    );
+    logger.action(`Creating ${shipsBulk.length} ships`);
+    return models.EveShips.bulkCreate(shipsBulk);
   });
 }
