de3eab857faa29269314bc6d8a3313fc9d167fe8
msaracevic <msaracevic@users.noreply.github.com>
2018-10-20 03:24:54 +0200

    Feat: Add empty slot holders for fitting simulator
    - also rewrite backend retrieval function to be more expensive on memory side, but less expensive in number of database queries (grab all then match by id instead of grabbing 200+ results by id)

--- frontend/src/containers/eve-fitting-tool/ShipView.js
+++ frontend/src/containers/eve-fitting-tool/ShipView.js
@@ -18,11 +18,45 @@ class ShipView extends Component {
     });
   }
 
-  renderDogmaAttribute(attribute) {
-    let ship = this.props.eveFittingSimulatorReducer;
+  renderDogmaAttribute(current = null, attribute) {
+    let dogma = this.props.eveFittingSimulatorReducer.dogmaAttributesNamed,
+        currentValue;
+
+    if (!dogma[attribute]) return null;
+
+    let attributeValue = dogma[attribute].value,
+        units          = dogma[attribute].units;
+
+    if (current) currentValue = dogma[current].value;
+    if (!isNaN(currentValue)) currentValue = currentValue.toLocaleString('de-DE');
+    if (!isNaN(attributeValue)) attributeValue = attributeValue.toLocaleString('de-DE');
+
+    return (
+      <div className='fitting__attribute'>
+        <img className='fitting__icon' src={`/images/eve/icons/${attribute.replace(' ', '')}.png`} alt='icon' title={attribute} />
+        {current ? `${currentValue} / ${attributeValue} ${units}` : `${attributeValue} ${units}`}
+      </div>
+    );
+  }
+
+  renderSlots(type) {
+    let slotNumber = this.props.eveFittingSimulatorReducer.dogmaAttributesNamed[type].value,
+        slots      = [];
+
+    for (let index = 0; index < slotNumber; index++) {
+      slots.push(<p key={index}>
+        Empty slot
+      </p>);
+    }
+
     return (
       <React.Fragment>
-        {ship.dogmaAttributesNamed[attribute].value} {ship.dogmaAttributesNamed[attribute].units}
+        <h5 className={type === 'High Slots' ? `fitting__title fitting__title--small` : `fitting__title`}>
+          {type}
+        </h5>
+        {type === 'High Slots' ?
+          <div className='fitting__subtitle'>{this.renderDogmaAttribute(null, 'Turret Hardpoints')} {this.renderDogmaAttribute(null, 'Launcher Hardpoints')}</div> : null}
+        {slots}
       </React.Fragment>
     );
   }
@@ -39,16 +73,27 @@ class ShipView extends Component {
           </Link>
           <section className="ship-view__header">
             <div className={`ship-view__ship-image tech-level tech-level--${dogma['Meta Level'].value}`}>
-              <img src={`https://image.eveonline.com/Render/${ship.type_id}_128.png`} alt="ship"/>
+              <img src={`https://image.eveonline.com/Render/${ship.type_id}_128.png`} alt="ship" />
             </div>
             <div className="ship_view__info">
-              <h4>{ship.name}</h4>
-              <p>
-                CPU: {this.renderDogmaAttribute('CPU Output')}
-              </p>
-              <p>
-                CPU: {this.renderDogmaAttribute('Powergrid Output')}
-              </p>
+              <h4 className="ship-view__title">{ship.name}</h4>
+              <p dangerouslySetInnerHTML={{__html: ship.description}}></p>
+            </div>
+          </section>
+          <section className='fitting'>
+            <div className='fitting__equipment'>
+              <h5 className='fitting__title'>Equipment</h5>
+            </div>
+            <div className='fitting__slots'>
+              {this.renderSlots('High Slots')}
+              {this.renderSlots('Medium Slots')}
+              {this.renderSlots('Low Slots')}
+              {this.renderSlots('Rig Slots')}
+            </div>
+            <div className='fitting__stats'>
+              <h5 className='fitting__title'>Stats</h5>
+              {this.renderDogmaAttribute('CPU Load', 'CPU Output')}
+              {this.renderDogmaAttribute('Power Load', 'Powergrid Output')}
             </div>
           </section>
         </article>
--- server/routes/eve-fitting-simulator.js
+++ server/routes/eve-fitting-simulator.js
@@ -23,10 +23,10 @@ export function getShipGroup(req, res) {
         where: {id: ship.id}
       }).then(ship => {
         return {
-          name: ship.data.name,
-          type_id: ship.data.type_id,
+          name:       ship.data.name,
+          type_id:    ship.data.type_id,
           meta_level: ship.data.dogma_attributes.filter(attribute => attribute.attribute_id === 633)[0].value
-        }
+        };
       }))
   )).then(data => {
     data.sort(dynamicSortMultiple('meta_level', 'name'));
@@ -35,52 +35,56 @@ export function getShipGroup(req, res) {
 }
 
 export function getShip(req, res) {
-  let fullShipModel = null;
+  let shipData = null;
   models.EveShips.findOne({
     where: {
       id: req.body.id
     }
   }).then(ship => {
-    fullShipModel = ship.data;
+    shipData = ship.data;
+    shipData.dogmaAttributesNamed = {};
     // for each dogma attribute we need name and unit if it has it
     // keep the originals intact and create new object where we can reference
     // it by name
-    ship.data.dogmaAttributesNamed = {};
-    return Promise.all(ship.data.dogma_attributes.map((dogmaAttribute, index) => {
-      return models.EveDogmaAttributes.findOne({where: {id: dogmaAttribute.attribute_id}})
-        .then(dogmaAttribute => {
-          dogmaAttribute = dogmaAttribute.data;
+    return models.EveDogmaAttributes.findAll().then(dogmaAttributes => {
+      let dogmaAttributesById = {};
+      // from the database response, these are all defined attributes in game
+      // build in form of
+      // {actualId: actualData, ...}
+      dogmaAttributes.forEach(dogmaAttribute => {
+        dogmaAttributesById[dogmaAttribute.id] = dogmaAttribute.data;
+      });
 
-          let dogmaValue   = ship.data.dogma_attributes[index].value,
-              namedIndex   = dogmaAttribute.display_name,
-              rawAttribute = false;
+      // missing from API export... ccc...
+      dogmaAttributesById[1547].unit += ';4=capital';
 
-          if (!namedIndex || namedIndex === '') {
-            namedIndex   = dogmaAttribute.name;
-            rawAttribute = true;
-          }
+      // from the already known attributes on the ship, go for each and
+      // attach additional data from previous step
+      shipData.dogma_attributes.forEach(shipDogmaAttribute => {
+        let dogmaAttribute = dogmaAttributesById[shipDogmaAttribute.attribute_id],
+            dogmaValue     = shipDogmaAttribute.value,
+            namedIndex     = dogmaAttribute.display_name,
+            dogmaIndexRef  = dogmaAttribute.attribute_id;
 
-          ship.data.dogmaAttributesNamed[namedIndex] = {
-            value:        dogmaValue,
-            rawAttribute: rawAttribute,
-          };
+        if (!namedIndex || namedIndex === '') namedIndex = dogmaAttribute.name;
 
-          if (dogmaAttribute.unit_id) {
-            let unit = units(dogmaAttribute.unit_id);
-            // yea, well, good luck figuring this out in a month...
-            if (unit) {
-              // if unit is in form of "1=small;2=medium;3=large"
-              if (unit.indexOf(';') !== -1) {
-                (unit.split(';')).forEach(part => {
-                  if (part.indexOf(dogmaValue) !== -1) unit = (part.split('='))[1];
-                });
-              }
-              ship.data.dogmaAttributesNamed[namedIndex].units = unit;
-            }
-          }
-        });
-    }));
+        shipData.dogmaAttributesNamed[namedIndex] = {
+          value: dogmaValue,
+          index: dogmaIndexRef
+        };
+
+        let unit = units(dogmaAttribute.unit_id);
+        // yea, well, good luck figuring this out in a month...
+        if (unit) {
+          // if unit is in form of "1=small;2=medium;3=large"
+          unit.split(';').forEach(part => {
+            if (part.indexOf(dogmaValue) !== -1) unit = part.split('=')[1];
+          });
+          shipData.dogmaAttributesNamed[namedIndex].units = unit;
+        }
+      });
+    });
   }).then(() => {
-    res.json(fullShipModel);
+    res.json(shipData);
   });
 }
