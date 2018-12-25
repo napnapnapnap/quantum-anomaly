7ab214fe8a5953733cf553416ca599e432341792
Miroslav Saracevic <msaracevic@users.noreply.github.com>
2018-12-25 19:47:33 +0100

    Feat: Add SEO function to update title and description

---
+++ frontend/src/helpers/seo.js
@@ -0,0 +1,9 @@
+export function seo(data = {}) {
+  data.title           = data.title || 'Quantum Anomaly';
+  data.metaDescription = data.metaDescription || 'Quantum Anomaly websites contains useful information and resources about online games such as EVE Online and WarFrame';
+
+  if (data.title.indexOf('Quantum Anomaly') === -1) data.title += ' - Quantum Anomaly';
+
+  document.title = data.title;
+  document.querySelector('meta[name="description"]').setAttribute('content', data.metaDescription);
+}
